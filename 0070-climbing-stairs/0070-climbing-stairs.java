d003852697bf1ccbea7356d8206b874d3d917b7b
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2026-07-27 16:26:50 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.65%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+if (n == 1) {
+            return 1;
+        }
+if (n == 2) {
+            return 2;
+        }
+
+        int first = 1;
+        int second = 2;
+        int ans = 0;
+
+for (int i = 3; i <= n; i++) {
+  ans = first + second;
+   first = second;
+  second = ans;
+        }
+return ans;
+    }
+}
